8a3a4b14789e968e49c5a8f0d4d63ba93e7e8094
Rachid Agbandou <rachdev@MacBook-Pro-de-Rachid.local>
2026-02-28 11:08:53 +0100

    Correction anciennes ressources JS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    // Cible uniquement les navigateurs modernes (supprime les polyfills legacy inutiles)
+    legacyBrowsers: false,
+  },
 }
 
 module.exports = nextConfig
